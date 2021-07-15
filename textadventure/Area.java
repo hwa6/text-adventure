dfd63a346fc2736b7ddc34077c71ff2c67715ee7
Henry Anderson <handerson5486@gmail.com>
2021-07-15 05:26:08 -0400

    revamped navigator, added mechanics

--- textadventure/Area.java
+++ textadventure/Area.java
@@ -3,11 +3,15 @@ public class Area {
     private String name;
     private String description;
     private String damageDescription;
+    private String unlockDescription;
     private Integer damage;
     private boolean firstVisit;
-    private boolean isLast;
+    private boolean breakNav;
     private Item item;
     private Area[] subAreas;
+    private boolean isLocked;
+    private KeyItem lockKey;
+    private String lockPassword;
   
     //constructor
     //area with no Item
@@ -16,37 +20,22 @@ public class Area {
         this.name=name;
         this.description=description;
     }
-    //area with Item
-    public Area(String name, String description, Item item){
-        firstVisit=true;
-        this.name=name;
-        this.item=item;
-        this.description=description;
-    }
 
-    //area with np Item that is "last area"
-    public Area(String name, String description, boolean isLast){
-        firstVisit=true;
-        this.name=name;
-        this.description=description;
-        this.isLast=isLast;
-    }
-    //area with Item that is "last area"
-    public Area(String name, String description, Item item, boolean isLast){
-        firstVisit=true;
-        this.name=name;
-        this.item=item;
-        this.description=description;
-        this.isLast=isLast;
-    }
-
-      //area with no Iteam that damages player
-      public Area(String name, String description, int damage, String damageDescription){
+    //area with no Item that damages player
+    public Area(String name, String description, int damage, String damageDescription){
         firstVisit=true;
         this.name=name;
         this.description=description;
         this.damage=damage;
         this.damageDescription=damageDescription;
+    }
+
+    //area with no Item that breaks nav
+    public Area(String name, String description, boolean breakNav){
+        firstVisit=true;
+        this.name=name;
+        this.description=description;
+        this.breakNav=breakNav;
     }
 
     //area with no Item that contains "sub areas"
@@ -56,6 +45,94 @@ public class Area {
         this.description=description;
         this.subAreas=subAreas;
     }
+
+    //area with no Item that has KeyItem password
+    public Area(String name, String unlockDescription, KeyItem lockKey){
+        isLocked=true;
+        firstVisit=false;
+        this.name=name;
+        this.description="It's locked. We need to find a key.";
+        this.lockKey=lockKey;
+        this.unlockDescription=unlockDescription;
+    }
+
+    public void unlockRoom(KeyItem lockKey){
+        if(this.lockKey.equals(lockKey)){
+            System.out.println("Aha! The " + lockKey.getName() + " unlocked the area.");
+            firstVisit=true;
+            isLocked=false;
+            this.description=this.unlockDescription;
+        }
+        else{
+            System.out.println("You don't have the correct item.");
+        }
+    }
+
+    
+
+    //area with no Item that has String password
+    public Area(String name, String unlockDescription, String lockPassword){
+        isLocked=true;
+        firstVisit=false;
+        this.name=name;
+        this.description="It's locked. We need to find the password.";
+        this.lockPassword=lockPassword;
+        this.unlockDescription=unlockDescription;
+    }
+
+    public void unlockRoom(String lockPassword){
+        if(this.lockPassword.equals(lockPassword)){
+            System.out.println("Aha! The password was "+ lockPassword + ". Area unlocked.");
+            firstVisit=true;
+            isLocked=false;
+            this.description=this.unlockDescription;
+        }
+        else{
+            System.out.println("Nope, that password didn't work.");
+        }
+    }
+
+     //area with Item
+     public Area(String name, String description, Item item){
+        firstVisit=true;
+        this.name=name;
+        this.item=item;
+        this.description=description;
+    }
+
+    //area with Item that damages player
+    public Area(String name, String description, Item item, int damage, String damageDescription){
+        firstVisit=true;
+        this.name=name;
+        this.description=description;
+        this.item=item;
+        this.damage=damage;
+        this.damageDescription=damageDescription;
+    }    
+
+    //area with Item that breaks nav
+    public Area(String name, String description, Item item, boolean breakNav){
+        firstVisit=true;
+        this.name=name;
+        this.item=item;
+        this.description=description;
+        this.breakNav=breakNav;
+    }
+
+    //area with Item that damages player, break nav
+    public Area(String name, String description, Item item, int damage, String damageDescription, boolean breakNav){
+        firstVisit=true;
+        this.name=name;
+        this.description=description;
+        this.item=item;
+        this.damage=damage;
+        this.damageDescription=damageDescription;
+        this.breakNav=breakNav;
+    }
+
+
+
+    
 
     //getter setters
     public String getName(){
@@ -74,13 +151,8 @@ public class Area {
         firstVisit=false;
     }
 
-    public boolean beenVisited(){
-        if(firstVisit==true){
-            return false;
-        }
-        else{
-            return true;
-        }
+    public boolean firstVisit(){
+       return firstVisit;
     }
 
     public boolean hasItem(){
@@ -92,8 +164,8 @@ public class Area {
         }
     }
 
-    public boolean isLast(){
-        return isLast;
+    public boolean breakNav(){
+        return breakNav;
     }
 
     public boolean hasSubAreas(){
@@ -124,6 +196,19 @@ public class Area {
 
     public String getDamageDescription(){
         return damageDescription;
+    }
+
+    public boolean isLocked(){
+        return isLocked;
+    }
+
+    public boolean hasPasswordLock(){
+        if(lockPassword==null){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 
 }
